a024ee34b331731831b674640c65ca3fbc4518ad
Siegmund <amceog88@gmail.com>
2017-12-08 13:03:38 +0800

    瀏覽餐廳資料

--- app/controllers/eichhorn/restaurants_controller.rb
+++ app/controllers/eichhorn/restaurants_controller.rb
@@ -10,6 +10,11 @@ class Eichhorn::RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
