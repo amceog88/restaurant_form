5207da979c7052e73853aa288ec4e71815aa6468
Siegmund <amceog88@gmail.com>
2017-12-08 11:57:20 +0800

    新增餐廳資料及表單驗證

--- app/controllers/eichhorn/restaurants_controller.rb
+++ app/controllers/eichhorn/restaurants_controller.rb
@@ -5,5 +5,26 @@ class Eichhorn::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
-  
-end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "restaurant was successfully created"
+      redirect_to eichhorn_restaurants_path
+    else
+      flash.now[:alert] = "restaurant was failed to create"
+      render :new
+    end
+  end
+
+  private
+
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+    end
+  end
+
