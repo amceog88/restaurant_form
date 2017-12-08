7913155869bed8a49c6f5b1f52f054d19cd65a17
Siegmund <amceog88@gmail.com>
2017-12-08 13:39:09 +0800

    編輯餐廳資料

--- app/controllers/eichhorn/restaurants_controller.rb
+++ app/controllers/eichhorn/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class Eichhorn::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_eichhorn
+  before_action :set_restaurant, only:[:show, :edit, :update]
 
   def index
     @restaurants = Restaurant.all
@@ -9,11 +10,6 @@ class Eichhorn::RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
-
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
-
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
@@ -26,10 +22,24 @@ class Eichhorn::RestaurantsController < ApplicationController
     end
   end
 
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to admin_restaurant_path(@restaurant)
+      flash[:notice] = "restaurant was successfully updated"
+    else
+      render :edit
+      flash[:alert] = "restaurant was failed to update"
+    end
+  end
+
   private
 
     def restaurant_params
       params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description)
+    end
+
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
     end
   end
 
