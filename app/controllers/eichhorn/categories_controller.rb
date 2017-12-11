49a96e5e3f0e25152bdfedab9706e2879b3adb72
Siegmund <amceog88@gmail.com>
2017-12-11 15:25:04 +0800

    重構::CategoriesController

--- app/controllers/eichhorn/categories_controller.rb
+++ app/controllers/eichhorn/categories_controller.rb
@@ -1,12 +1,12 @@
 class Eichhorn::CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_eichhorn
+  before_action :set_category, only:[:update, :destroy]
 
   def index
     @categories = Category.all
-
     if params[:id]
-      @category = Category.find(params[:id])
+      set_category
     else
       @category = Category.new
     end
@@ -25,7 +25,7 @@ class Eichhorn::CategoriesController < ApplicationController
   end
 
   def update
-    @category = Category.find(params[:id])
+    
     if @category.update(category_params)
       redirect_to eichhorn_categories_path
       flash[:notice] = "category was successfully updated"
@@ -36,7 +36,7 @@ class Eichhorn::CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id])
+    
     @category.destroy
     flash[:alert] = "category was successfully deleted"
     redirect_to eichhorn_categories_path
@@ -46,5 +46,9 @@ class Eichhorn::CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
