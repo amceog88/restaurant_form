3812585e1b62c51d54957d4c1263425f0877fc41
Siegmund <amceog88@gmail.com>
2017-12-11 13:28:18 +0800

    新增分類

--- app/controllers/eichhorn/categories_controller.rb
+++ app/controllers/eichhorn/categories_controller.rb
@@ -4,5 +4,24 @@ class Eichhorn::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "分類設置完成"
+      redirect_to eichhorn_categories_path
+
+    else
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
