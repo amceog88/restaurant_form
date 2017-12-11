b18055f6dcb28cfedb8c007d0a2f8bae6c16e389
Siegmund <amceog88@gmail.com>
2017-12-11 12:07:23 +0800

    瀏覽分類

--- app/controllers/eichhorn/categories_controller.rb
+++ app/controllers/eichhorn/categories_controller.rb
@@ -1,4 +1,8 @@
 class Eichhorn::CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_eichhorn
+
+  def index
+    @categories = Category.all
+  end
 end
