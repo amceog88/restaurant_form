d2fe74dc6716f180878b92a92e08af91629bfca7
Siegmund <amceog88@gmail.com>
2017-12-12 14:23:01 +0800

    依分頁瀏覽餐廳_設定路由

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-
-def show
-  @restaurant = Restaurant.find(params[:id])
-end
-
 end
